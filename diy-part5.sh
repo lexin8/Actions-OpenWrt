8644d493304b7d7b3b79649a84a1b3194d37ab1e
lexin8 <61616502+lexin8@users.noreply.github.com>
2020-12-06 15:21:49 +0800

    Create diy-part5.sh

---
+++ diy-part5.sh
@@ -0,0 +1,6 @@
+
+
+svn checkout https://github.com/coolsnowwolf/lede/trunk/package/lean/microsocks
+svn checkout https://github.com/coolsnowwolf/lede/trunk/package/lean/redsocks2
+svn checkout https://github.com/coolsnowwolf/lede/trunk/package/lean/v2ray
+svn checkout https://github.com/coolsnowwolf/lede/trunk/package/lean/xray
