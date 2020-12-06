531a8816a36c5d0f0c8006ba949e1513c03b1f5c
lexin8 <61616502+lexin8@users.noreply.github.com>
2020-12-06 15:43:05 +0800

    Update diy-part5.sh

--- diy-part5.sh
+++ diy-part5.sh
@@ -1,6 +1,9 @@
-
-
+#!/bin/bash
+echo "\n----------- 开始进入指定文件夹 --------------\n";
+cd package/lede/
+rm -rf luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
 svn checkout https://github.com/coolsnowwolf/lede/trunk/package/lean/microsocks
 svn checkout https://github.com/coolsnowwolf/lede/trunk/package/lean/redsocks2
 svn checkout https://github.com/coolsnowwolf/lede/trunk/package/lean/v2ray
 svn checkout https://github.com/coolsnowwolf/lede/trunk/package/lean/xray
+cd ../ && git clone https://github.com/fw876/helloworld.git
