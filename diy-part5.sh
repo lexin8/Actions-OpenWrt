4d561d53434f85c2a43bdd9bc08a0bb314794f86
lexin8 <61616502+lexin8@users.noreply.github.com>
2020-12-10 00:19:58 +0800

    Update diy-part5.sh

--- diy-part5.sh
+++ diy-part5.sh
@@ -9,11 +9,12 @@ svn checkout https://github.com/kenzok8/openwrt-packages/trunk/luci-theme-opento
 svn checkout https://github.com/kenzok8/openwrt-packages/trunk/luci-theme-opentomcat
 svn checkout https://github.com/kenzok8/openwrt-packages/trunk/luci-theme-ifit
 # 删除配置
-sed -i '/luci.main.mediaurlbase/d' luci-theme-atmaterial-ColorIcon/root/etc/uci-defaults/30_luci-theme-atmaterial_ci
-sed -i '/luci.main.mediaurlbase/d' luci-theme-Light/luci-theme-Light/root/etc/uci-defaults/luci-theme-Light
-sed -i '/luci.main.mediaurlbase/d' luci-theme-opentopd/root/etc/uci-defaults/30_luci-theme-opentopd
-sed -i '/luci.main.mediaurlbase/d' luci-theme-opentomcat/files/30_luci-theme-opentomcat
-sed -i '/luci.main.mediaurlbase/d' luci-theme-ifit/files/10_luci-theme-ifit
+grep -rnl 'luci.main.mediaurlbase' ./ | xargs sed -i '/luci.main.mediaurlbase/d'
+# sed -i '/luci.main.mediaurlbase/d' luci-theme-atmaterial-ColorIcon/root/etc/uci-defaults/30_luci-theme-atmaterial_ci
+# sed -i '/luci.main.mediaurlbase/d' luci-theme-Light/luci-theme-Light/root/etc/uci-defaults/luci-theme-Light
+# sed -i '/luci.main.mediaurlbase/d' luci-theme-opentopd/root/etc/uci-defaults/30_luci-theme-opentopd
+# sed -i '/luci.main.mediaurlbase/d' luci-theme-opentomcat/files/30_luci-theme-opentomcat
+# sed -i '/luci.main.mediaurlbase/d' luci-theme-ifit/files/10_luci-theme-ifit
 # 添加插件
 svn checkout https://github.com/kenzok8/openwrt-packages/trunk/luci-app-jd-dailybonus
 # 添加依赖
