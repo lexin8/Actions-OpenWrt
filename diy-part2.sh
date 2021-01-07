f1f85bb5c5d1ed77dbceb26c6e09795f3519233a
lexin8 <61616502+lexin8@users.noreply.github.com>
2021-01-07 10:44:25 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,33 +9,7 @@
 # Blog: https://p3terx.com
 #============================================================
 
-# Modify default IP
-# sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
-# Delete default password
-# sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/default-settings/files/zzz-default-settings
-# 修改主机名字，把OpenWrt-123修改你喜欢的就行（不能使用中文）
-# sed -i '/uci commit system/i\uci set system.@system[0].hostname='OpenWrt-123'' package/default-settings/files/zzz-default-settings
-
-# 内核显示增加自己个性名称（281677160 build $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些为后期增加）
-sed -i "s/19.07-SNAPSHOT/19.07 build $(TZ=UTC-8 date "+%Y.%m.%d") /g" package/default-settings/files/zzz-default-settings
-
-# 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
-# sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-
-# 修改内核版本
-#sed -i 's/KERNEL_PATCHVER:=4.14/KERNEL_PATCHVER:=4.19/g' target/linux/x86/Makefile
-
-# 修改插件名字（修改名字后不知道会不会对插件功能有影响，自己多测试）
-# sed -i '/msgstr/ { s/ShadowSocksR Plus+ 设置/科学上网设置/g;  s/ShadowSocksR/科学上网/g; }' package/helloworld/luci-app-ssr-plus/po/zh-cn/ssr-plus.po
-# sed -i 's/"ShadowSocksR Plus+"/"科学上网"/g' package/helloworld/luci-app-ssr-plus/luasrc/controller/shadowsocksr.lua
-# sed -i "s#ShadowsocksR Plus+ #科学上网程序#g" package/helloworld/luci-app-ssr-plus/luasrc/view/shadowsocksr/status.htm
-# sed -i 's/"PassWall"/"正确翻墙姿势✔"/g' feeds/diy1/luci-app-passwall/po/zh-cn/passwall.po
-
-find . -name ssr-plus.po | xargs sed -i '/msgstr/ { s/ShadowSocksR Plus+ 设置/科学上网设置/g;  s/ShadowSocksR/科学上网/g; }'
-find . -name shadowsocksr.lua | xargs sed -i 's/"ShadowSocksR Plus+"/"科学上网"/g'
-find . -name status.htm | xargs sed -i "s#ShadowsocksR Plus+ #科学上网程序#g"
-find . -name passwall.po | xargs sed -i 's/"PassWall"/"正确翻墙姿势✔"/g'
 
 # sed -i 's/PassWall/正确翻墙翻墙✔/g' package/openwrt-packages/luci-app-passwall/Makefile
 # sed -i 's/"BaiduPCS Web"/"百度网盘"/g' package/lean/luci-app-baidupcs-web/luasrc/controller/baidupcs-web.lua
