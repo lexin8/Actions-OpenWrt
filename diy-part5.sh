d565dfed15ed8d1659548643ae8f52489c54f1a7
lexin8 <61616502+lexin8@users.noreply.github.com>
2021-01-24 21:22:15 +0800

    Update diy-part5.sh

--- diy-part5.sh
+++ diy-part5.sh
@@ -22,6 +22,6 @@ svn checkout https://github.com/kenzok8/openwrt-packages/trunk/luci-app-jd-daily
 svn checkout https://github.com/coolsnowwolf/lede/trunk/package/lean/microsocks
 svn checkout https://github.com/coolsnowwolf/lede/trunk/package/lean/redsocks2
 svn checkout https://github.com/coolsnowwolf/lede/trunk/package/lean/v2ray
-svn checkout https://github.com/coolsnowwolf/lede/trunk/package/lean/xray
+svn checkout https://github.com/lexin8/lede/trunk/package/lean/xray
 cd ../ && git clone https://github.com/fw876/helloworld.git
 # cd ../ && svn checkout https://github.com/kenzok8/openwrt-packages/trunk/luci-app-ssr-plus
