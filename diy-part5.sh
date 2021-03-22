63df70074c1392ab1673657773164a4aa8deaebd
lexin8 <61616502+lexin8@users.noreply.github.com>
2021-03-22 21:18:29 +0800

    Update diy-part5.sh

--- diy-part5.sh
+++ diy-part5.sh
@@ -18,7 +18,7 @@ grep -rnl 'luci.main.mediaurlbase' ./ | xargs sed -i '/luci.main.mediaurlbase/d'
 # sed -i '/luci.main.mediaurlbase/d' luci-theme-ifit/files/10_luci-theme-ifit
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
 # 添加插件
-svn checkout https://github.com/kenzok8/openwrt-packages/trunk/luci-app-jd-dailybonus
+# svn checkout https://github.com/kenzok8/openwrt-packages/trunk/luci-app-jd-dailybonus
 svn checkout https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-uugamebooster
 svn checkout https://github.com/coolsnowwolf/lede/trunk/package/lean/uugamebooster
 # 添加依赖
