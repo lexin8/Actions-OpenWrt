9d1b471c8188e53ee9069caf54f0555763708746
lexin8 <61616502+lexin8@users.noreply.github.com>
2021-04-09 21:33:35 +0800

    Update diy-lede4.sh

--- diy-lede4.sh
+++ diy-lede4.sh
@@ -16,6 +16,7 @@ grep -rnl 'luci.main.mediaurlbase' ./ | xargs sed -i '/luci.main.mediaurlbase/d'
 # sed -i '/luci.main.mediaurlbase/d' luci-theme-opentomcat/files/30_luci-theme-opentomcat
 # sed -i '/luci.main.mediaurlbase/d' luci-theme-ifit/files/10_luci-theme-ifit
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
+git clone https://github.com/jerrykuku/luci-app-argon-config
 git clone https://github.com/jerrykuku/lua-maxminddb.git
 git clone https://github.com/jerrykuku/luci-app-vssr.git
 # 添加插件
