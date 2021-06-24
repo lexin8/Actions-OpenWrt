253e87c025bf8e0b72f8b2ba5406e03618fedd78
lexin8 <61616502+lexin8@users.noreply.github.com>
2021-06-24 17:32:27 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@ echo "\n----------- 开始进入指定文件夹 --------------\n";
 #拉取源码
 git clone https://github.com/coolsnowwolf/lede openwrt
 cd openwrt
+sed -i '$a src-git diy1 https://github.com/xiaorouji/openwrt-passwall.git;main' feeds.conf.default
 ./scripts/feeds update -a && ./scripts/feeds install -a
 pushd package/lean
 # 添加主题
