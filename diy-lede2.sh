a87951f9e88d4b921b697f66594667e2c4977985
lexin8 <61616502+lexin8@users.noreply.github.com>
2021-07-30 18:39:07 +0800

    Update diy-lede2.sh

--- diy-lede2.sh
+++ diy-lede2.sh
@@ -38,7 +38,9 @@ find . -name shadowsocksr.lua | xargs sed -i 's/"ShadowSocksR Plus+"/"科学上�
 find . -name status.htm | xargs sed -i "s#ShadowsocksR Plus+ #科学上网程序#g"
 #find . -name passwall.po | xargs sed -i 's/"PassWall"/"帕斯沃"/g'
 #find . -name vssr.lua | xargs sed -i "s/Hello World/科学上网国旗版/g"
-#find . -name vssr.po | xargs sed -i '/msgstr/s/Hello World/科学上网国旗版/g'
+#find . -name vssr.po | xargs sed -i '/msgstr/s/Hello World/SSR+国旗版/g'
+#find . -name bypass.lua | xargs sed -i "s/Bypass/科学上网国旗版/g"
+#find . -name bypass.po | xargs sed -i '/msgstr/s/Bypass/SSR+国旗版/g'
 
 # sed -i 's/PassWall/正确翻墙翻墙✔/g' package/openwrt-packages/luci-app-passwall/Makefile
 # sed -i 's/"BaiduPCS Web"/"百度网盘"/g' package/lean/luci-app-baidupcs-web/luasrc/controller/baidupcs-web.lua
