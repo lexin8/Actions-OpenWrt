447e65abb27eb20c0f69d8573aff7f2cc6d1ef07
lexin8 <61616502+lexin8@users.noreply.github.com>
2021-09-04 11:56:17 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -34,7 +34,7 @@ popd
 # 添加插件
 cd package && git clone https://github.com/fw876/helloworld
 git clone https://github.com/tuanqing/install-program
-# git clone https://github.com/sirpdboy/NetSpeedTest
+git clone https://github.com/sirpdboy/NetSpeedTest
 # svn checkout https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-koolproxyR
  cd ../ && rm -rf feeds/diy1/v2ray
 rm -rf package/kenzok8
