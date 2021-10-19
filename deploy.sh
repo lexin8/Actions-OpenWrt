d06662603a012eed11c59ff14e6b6a4785585b44
lexin8 <61616502+lexin8@users.noreply.github.com>
2021-10-19 11:45:18 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -36,7 +36,7 @@ cd package
 #&& git clone https://github.com/fw876/helloworld
 #git clone https://github.com/tuanqing/install-program
 #git clone https://github.com/sirpdboy/netspeedtest
-git clone https://github.com/waynesg/luci-app-netspeedtest
+#git clone https://github.com/waynesg/luci-app-netspeedtest
 # svn checkout https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-koolproxyR
 cd ../
 #&& rm -rf feeds/diy1/v2ray
