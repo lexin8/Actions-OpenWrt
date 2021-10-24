a021d2b9490c04a44e35b46c4b2da8c5336df890
lexin8 <61616502+lexin8@users.noreply.github.com>
2021-10-24 23:20:28 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "\n----------- 开始进入指定文件夹 --------------\n";
 #拉取源码
+git clone https://github.com/lexin8/kernel opt
 git clone https://github.com/tuanqing/mknop
 git clone https://github.com/coolsnowwolf/lede openwrt
 cd openwrt
