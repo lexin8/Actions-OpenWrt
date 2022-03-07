056ddc9a44ba55f996c2491a652a66571da5d509
lexin8 <61616502+lexin8@users.noreply.github.com>
2022-03-07 16:16:01 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ cd openwrt
 ./scripts/feeds install -a
 git clone https://github.com/kenzok8/openwrt-packages.git package/kenzok8
 rm -rf feeds/luci/applications/luci-theme*
-rm -rf feeds/luci/themes/luci-theme-argon
+rm -rf feeds/luci/themes/luci-theme-bootstrap
 pushd package/lean
 # 添加主题
 rm -rf luci-theme*
