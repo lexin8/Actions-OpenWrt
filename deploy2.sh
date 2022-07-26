a6fd2e0a5f8a89c048fe37f92d68beb5b2321cd9
lexin8 <61616502+lexin8@users.noreply.github.com>
2022-07-26 09:52:10 +0800

    Update deploy2.sh

--- deploy2.sh
+++ deploy2.sh
@@ -8,37 +8,11 @@ cd openwrt
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 git clone https://github.com/kenzok8/openwrt-packages.git package/kenzok8
-rm -rf feeds/luci/themes/luci-theme*
-cd feeds/luci/themes/
-ls |grep -v luci-theme-bootstrap |xargs rm -rf && cd -
 pushd feeds/luci/themes
 # 添加主题
-rm -rf luci-theme*
-# rm -rf luci-lib-docker
-# rm -rf luci-app-diskman
-# svn co https://github.com/lisaac/luci-app-dockerman/trunk/applications/luci-app-dockerman
-# svn co https://github.com/lisaac/luci-app-diskman/trunk/applications/luci-app-diskman
-# git clone https://github.com/lisaac/luci-lib-docker
-git clone https://github.com/esirplayground/luci-theme-atmaterial-ColorIcon
-git clone https://github.com/Aslin-Ameng/luci-theme-Light
-git clone https://github.com/sirpdboy/luci-theme-opentopd
-git clone -b 18.06 https://github.com/kiddin9/luci-theme-edge.git
-git clone https://github.com/virualv/luci-theme-pink.git
-git clone https://github.com/thinktip/luci-theme-neobird.git
-git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git
-cp -r ~/work/Actions-OpenWrt/Actions-OpenWrt/openwrt/package/kenzok8/luci-app-openclash .
-cp -r ~/work/Actions-OpenWrt/Actions-OpenWrt/openwrt/package/kenzok8/luci-theme-opentomcat .
-cp -r ~/work/Actions-OpenWrt/Actions-OpenWrt/openwrt/package/kenzok8/luci-theme-ifit .
+cp -r ~/work/Actions-OpenWrt/Actions-OpenWrt/openwrt/package/kenzok8/luci-app-openclash ..
 cp -r ~/work/Actions-OpenWrt/Actions-OpenWrt/openwrt/package/kenzok8/luci-app-bypass .
 cp -r ~/work/Actions-OpenWrt/Actions-OpenWrt/openwrt/package/kenzok8/smartdns-le .
-# 删除配置
-grep -rnl 'luci.main.mediaurlbase' ./ | xargs sed -i '/luci.main.mediaurlbase/d'
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
-# git clone https://github.com/jerrykuku/luci-theme-argon.git
-# git clone https://github.com/jerrykuku/luci-app-argon-config
-git clone https://github.com/jerrykuku/lua-maxminddb.git
-git clone https://github.com/jerrykuku/luci-app-vssr.git
-# svn checkout https://github.com/kenzok8/openwrt-packages/trunk/luci-app-openclash
 popd
 # 添加插件
 cd package && git clone https://github.com/fw876/helloworld
