dc4efc2720b404abd9dd0fdca6450c1b713e0b74
lexin8 <61616502+lexin8@users.noreply.github.com>
2022-08-19 23:54:01 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ git clone https://github.com/lexin8/kernel opt
 git clone https://github.com/tuanqing/mknop
 git clone https://github.com/coolsnowwolf/lede openwrt
 cd openwrt
-git reset --hard a0cac9a
+# git reset --hard a0cac9a
 # sed -i '$a src-git diy1 https://github.com/xiaorouji/openwrt-passwall.git;main' feeds.conf.default
 ./scripts/feeds update -a
 ./scripts/feeds install -a
