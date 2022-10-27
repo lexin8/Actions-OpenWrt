e62bab9fcc541f04493a974368ddd39e6d138d06
lexin8 <61616502+lexin8@users.noreply.github.com>
2022-10-27 11:28:24 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ echo "\n----------- 开始进入指定文件夹 --------------\n";
 git clone https://github.com/lexin8/kernel opt
 git clone https://github.com/coolsnowwolf/lede openwrt
 cd openwrt
-# git reset --hard a0cac9a
+git reset --hard 2a91960
 # sed -i '$a src-git diy1 https://github.com/xiaorouji/openwrt-passwall.git;main' feeds.conf.default
 ./scripts/feeds update -a
 ./scripts/feeds install -a
