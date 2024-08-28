3c04d6957be01988164f88e393c3d2bea3367753
lexin8 <61616502+lexin8@users.noreply.github.com>
2024-08-28 20:44:39 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,7 @@ echo "\n----------- 开始进入指定文件夹 --------------\n";
 cd openwrt
 # git reset --hard f833707a78974af47ddbe1f7e038bf62b463f633
 # sed -i '$a src-git diy1 https://github.com/xiaorouji/openwrt-passwall.git;main' feeds.conf.default
+echo "src-git mihomo https://github.com/morytyann/OpenWrt-mihomo.git;main" >> "feeds.conf.default"
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 # 退回 packages
