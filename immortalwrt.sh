3299c7d5a513c6680e8979e70a5fc6272159e92d
lexin8 <61616502+lexin8@users.noreply.github.com>
2024-09-29 16:05:36 +0800

    Update immortalwrt.sh

--- immortalwrt.sh
+++ immortalwrt.sh
@@ -10,13 +10,6 @@ sed -i 's/192.168.1.1/192.168.10.253/g' package/base-files/files/bin/config_gene
 
 #cd package && git clone https://github.com/fw876/helloworld.git
 
-sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
-./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
-rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
-rm -rf feeds/packages/utils/v2dat
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
-./scripts/feeds install -a
+
 
 
