c75e6bc560e843c13e37ddc11fdd26c8593568d0
lexin8 <61616502+lexin8@users.noreply.github.com>
2024-09-29 10:15:33 +0800

    Create immortalwrt.sh

---
+++ immortalwrt.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "\n----------- 开始进入指定文件夹 --------------\n";
+#拉取源码
+# git clone https://github.com/lexin8/kernel opt
+cd openwrt
+# git reset --hard f833707a78974af47ddbe1f7e038bf62b463f633
+sed -i 's/192.168.1.1/192.168.10.253/g' package/base-files/files/bin/config_generate
+./scripts/feeds update -a
+./scripts/feeds install -a
+
+cd package && git clone https://github.com/fw876/helloworld.git
+
+
