7e9f4dd2961d0362a29d499e94d510ac477ca49a
lexin8 <61616502+lexin8@users.noreply.github.com>
2024-10-01 13:38:00 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -84,12 +84,3 @@ cd ../
 rm -rf package/kenzok8
 rm -rf package/liudf0716
 rm -rf package/kiddin9
-
-sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
-./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
-rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
-rm -rf feeds/packages/utils/v2dat
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
-./scripts/feeds install -a 
