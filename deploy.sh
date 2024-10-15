83e665b15710d19f517a3242ace52b5fc4f9e6e2
lexin8 <61616502+lexin8@users.noreply.github.com>
2024-10-15 19:42:09 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,8 @@ cd openwrt
 git reset --hard 625dbc350539d424d44f5d0683b734435e2aa90f
 # sed -i '$a src-git diy1 https://github.com/xiaorouji/openwrt-passwall.git;main' feeds.conf.default
 # echo "src-git mihomo https://github.com/morytyann/OpenWrt-mihomo.git;main" >> "feeds.conf.default"
-# sed -i '$a src-git luci https://github.com/coolsnowwolf/luci.git;openwrt-23.05' feeds.conf.default
-# sed -i '2,3d' feeds.conf.default
+sed -i '$a src-git luci https://github.com/coolsnowwolf/luci.git;openwrt-23.05' feeds.conf.default
+sed -i '2,3d' feeds.conf.default
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
