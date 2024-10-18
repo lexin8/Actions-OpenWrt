f2671ab1a285539c13a8a8e5775ba5b7b1330347
lexin8 <61616502+lexin8@users.noreply.github.com>
2024-10-18 23:33:10 +0800

    Update istoreos.sh

--- istoreos.sh
+++ istoreos.sh
@@ -3,8 +3,8 @@ echo "\n----------- 开始进入指定文件夹 --------------\n";
 #拉取源码
 # git clone https://github.com/lexin8/kernel opt
 cd openwrt
-sed -i '$a src-git diy https://github.com/xiaorouji/openwrt-passwall.git;main' feeds.conf.default
-sed -i '$a src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' feeds.conf.default
+# sed -i '$a src-git diy https://github.com/xiaorouji/openwrt-passwall.git;main' feeds.conf.default
+# sed -i '$a src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' feeds.conf.default
 sed -i 's/192.168.1.1/192.168.10.253/g' package/base-files/files/bin/config_generate
 ./scripts/feeds update -a
 ./scripts/feeds install -a
