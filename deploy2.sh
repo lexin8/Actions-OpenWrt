89b980b7e1c164c0fa8a597b35e9b41f785b8a83
lexin8 <61616502+lexin8@users.noreply.github.com>
2024-10-21 21:10:57 +0800

    Update deploy2.sh

--- deploy2.sh
+++ deploy2.sh
@@ -1,14 +1,35 @@
 #!/bin/bash
 echo "\n----------- 开始进入指定文件夹 --------------\n";
-#拉取源码
-# git clone https://github.com/lexin8/kernel opt
-# git clone https://github.com/coolsnowwolf/lede openwrt
 cd openwrt
-# git reset --hard 625dbc350539d424d44f5d0683b734435e2aa90f
-# sed -i '$a src-git diy1 https://github.com/xiaorouji/openwrt-passwall.git;main' feeds.conf.default
-# echo "src-git mihomo https://github.com/morytyann/OpenWrt-mihomo.git;main" >> "feeds.conf.default"
-sed -i '$a src-git luci https://github.com/immortalwrt/luci.git;openwrt-23.05' feeds.conf.default
-sed -i '2,3d' feeds.conf.default
-
 ./scripts/feeds update -a
 ./scripts/feeds install -a
+git clone https://github.com/kenzok8/openwrt-packages.git package/kenzok8
+git clone https://github.com/liudf0716/luci.git package/liudf0716
+git clone https://github.com/kiddin9/openwrt-packages.git package/kiddin9
+rm -rf feeds/luci/applications/luci-theme*
+cd feeds/luci/themes/
+ls |grep -v luci-theme-bootstrap |xargs rm -rf && cd -
+pushd package/lean
+# 添加主题
+rm -rf luci-theme*
+cp -r ~/work/Actions-OpenWrt/Actions-OpenWrt/openwrt/package/kenzok8/luci-app-openclash .
+cp -r ~/work/Actions-OpenWrt/Actions-OpenWrt/openwrt/package/kenzok8/luci-app-clash .
+cp -r ~/work/Actions-OpenWrt/Actions-OpenWrt/openwrt/package/kenzok8/luci-app-wechatpush .
+cp -r ~/work/Actions-OpenWrt/Actions-OpenWrt/openwrt/package/kenzok8/luci-app-store .
+cp -r ~/work/Actions-OpenWrt/Actions-OpenWrt/openwrt/package/liudf0716/applications/luci-app-strongswan-swanctl .
+
+# 删除配置
+grep -rnl 'luci.main.mediaurlbase' ./ | xargs sed -i '/luci.main.mediaurlbase/d'
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
+git clone https://github.com/jerrykuku/luci-app-argon-config
+git clone https://github.com/jerrykuku/lua-maxminddb.git
+popd
+# 添加插件
+cd package && git clone https://github.com/fw876/helloworld.git
+git clone https://github.com/xiaorouji/openwrt-passwall.git
+
+cd ../
+#&& rm -rf feeds/diy1/v2ray
+rm -rf package/kenzok8
+rm -rf package/liudf0716
+rm -rf package/kiddin9
