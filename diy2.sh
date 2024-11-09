f58f4bcf8e1082e87624ccd80e9582630ee74cc8
lexin8 <61616502+lexin8@users.noreply.github.com>
2024-11-09 09:53:25 +0800

    Update diy2.sh

--- diy2.sh
+++ diy2.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 echo "\n----------- 开始进入指定文件夹 --------------\n";
 #拉取源码
+git clone https://github.com/kenzok8/small-package.git small
+cd openwrt
 sed -i 's/192.168.6.1/192.168.20.1/g' package/base-files/files/bin/config_generate
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 
-git clone https://github.com/kenzok8/small-package.git small
 cd package/
-cp -r ~/work/Actions-OpenWrt/Actions-OpenWrt/openwrt/small/luci-app-istorex .
-cp -r ~/work/Actions-OpenWrt/Actions-OpenWrt/openwrt/small/luci-app-quickstart .
-cp -r ~/work/Actions-OpenWrt/Actions-OpenWrt/openwrt/small/luci-app-store .
-cp -r ~/work/Actions-OpenWrt/Actions-OpenWrt/openwrt/small/luci-lib-taskd .
-cp -r ~/work/Actions-OpenWrt/Actions-OpenWrt/openwrt/small/quickstart .
-cp -r ~/work/Actions-OpenWrt/Actions-OpenWrt/openwrt/small/luci-lib-xterm .
-cp -r ~/work/Actions-OpenWrt/Actions-OpenWrt/openwrt/small/taskd .
-
-
-cd ../
-rm -rf small
+cp -r ~/work/Actions-OpenWrt/Actions-OpenWrt/small/luci-app-istorex .
+cp -r ~/work/Actions-OpenWrt/Actions-OpenWrt/small/luci-app-quickstart .
+cp -r ~/work/Actions-OpenWrt/Actions-OpenWrt/small/luci-app-store .
+cp -r ~/work/Actions-OpenWrt/Actions-OpenWrt/small/luci-lib-taskd .
+cp -r ~/work/Actions-OpenWrt/Actions-OpenWrt/small/quickstart .
+cp -r ~/work/Actions-OpenWrt/Actions-OpenWrt/small/luci-lib-xterm .
+cp -r ~/work/Actions-OpenWrt/Actions-OpenWrt/small/taskd .
