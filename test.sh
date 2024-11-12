ca3db8176371f904942fbf6320dc3c4cd160d0cc
lexin8 <61616502+lexin8@users.noreply.github.com>
2024-11-12 20:43:14 +0800

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "\n----------- 开始进入指定文件夹 --------------\n";
+#拉取源码
+git clone https://github.com/coolsnowwolf/packages packages
+cd openwrt
+sed -i 's/192.168.6.1/192.168.20.1/g' package/base-files/files/bin/config_generate
+
+
+cd package/net/
+rm -rf nfs-kernel-server
+cp -r ~/work/Actions-OpenWrt/Actions-OpenWrt/packages/package/net/nfs-kernel-server .
+
