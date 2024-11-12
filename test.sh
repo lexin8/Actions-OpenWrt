dd60f4f460815822f5f7babe8de56eb2796a26f7
lexin8 <61616502+lexin8@users.noreply.github.com>
2024-11-12 22:20:12 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "\n----------- 开始进入指定文件夹 --------------\n";
 #拉取源码
-git clone -b openwrt-21.02 https://github.com/immortalwrt/packages packages
+git clone -b openwrt-18.06-k5.4 https://github.com/immortalwrt/packages packages
 cd openwrt
 sed -i 's/192.168.6.1/192.168.20.1/g' package/base-files/files/bin/config_generate
 
