82be447ee291fe5483cc875ff92b8f29bb0f4b26
lexin8 <61616502+lexin8@users.noreply.github.com>
2024-11-22 09:46:38 +0800

    Update immortalwrt.sh

--- immortalwrt.sh
+++ immortalwrt.sh
@@ -15,7 +15,7 @@
 #sed -i 's/$(TARGET_DIR)) install/$(TARGET_DIR)) install --force-overwrite/' package/Makefile
 #sed -i 's/root:.*/root:$1$tTPCBw1t$ldzfp37h5lSpO9VXk4uUE\/:18336:0:99999:7:::/g' package/base-files/files/etc/shadow
 
-git clone -b openwrt-24.10 https://github.com/immortalwrt/luci diy
+git clone -b main https://github.com/kiddin9/kwrt-packages diy
 
 cd openwrt
 echo "src-git mihomo https://github.com/morytyann/OpenWrt-mihomo.git;main" >> "feeds.conf.default"
@@ -26,4 +26,4 @@ echo "src-git mihomo https://github.com/morytyann/OpenWrt-mihomo.git;main" >> "f
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 cd package
-cp -r ~/work/Actions-OpenWrt/Actions-OpenWrt/diy/applications/luci-app-strongswan-swanctl .
+cp -r ~/work/Actions-OpenWrt/Actions-OpenWrt/diy/luci-app-strongswan-swanctl .
